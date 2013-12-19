94a2df5648f0433e0f44b71abe9d4e4139e4a29e
James Finlay <jtfinlay@ualberta.ca>
2013-12-18 17:10:50 -0700

    First view.

---
+++ src/ca/ippetad/charades/ca/ippetad/charades/view/CategoriesActivity.java
@@ -0,0 +1,11 @@
+package ca.ippetad.charades.ca.ippetad.charades.view;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: James
+ * Date: 12/18/13
+ * Time: 5:10 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CategoriesActivity {
+}
